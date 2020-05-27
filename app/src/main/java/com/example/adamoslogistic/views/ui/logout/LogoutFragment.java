c5d76ec9c21a067c74e839868a3f775217ebf422
user <leha-antipin2001@mail.ru>
2020-05-27 10:31:38 +0700

    logout

--- app/src/main/java/com/example/adamoslogistic/views/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/adamoslogistic/views/ui/logout/LogoutFragment.java
@@ -1,5 +1,8 @@
 package com.example.adamoslogistic.views.ui.logout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +12,39 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.adamoslogistic.R;
+import com.example.adamoslogistic.generic.DB;
+import com.example.adamoslogistic.models.User;
+import com.example.adamoslogistic.views.LoginActivity;
 
 public class LogoutFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_logout, container, false);
-        return root;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setMessage("Вы действительно хотите выйти из приложения?")
+                .setTitle("Выход");
+
+        builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                try {
+                    DB.SetCurrentUser(new User());
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        builder.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+// User cancelled the dialog
+            }
+        });
+
+        builder.create().show();
     }
 }
